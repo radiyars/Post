f8817e7bffdf3298cc7949cc4550f7bb33200d7d
Radiy <radiyars@yandex.ru>
2023-07-09 10:29:23 +0300

    нестыковка по http/https

--- src/api/likes-api.ts
+++ src/api/likes-api.ts
@@ -5,12 +5,12 @@ import { instance } from './api'
 export const likesApi = {
 
 	getLikes() {
-		return instance.get<Array<PostType>>(`/`)
+		return instance.get<Array<PostType>>(``)
 			.then(response => response.data[0].likes);
 	},
 
 	patchLikes(id: string, likes: number) {
-		return instance.patch<PostType>(`/likes/${id}/`, { likes })
+		return instance.patch<PostType>(`/likes/${id}`, { likes })
 			.then(response => response.data);
 	}
 
